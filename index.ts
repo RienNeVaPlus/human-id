60c3594229d8b6e4d43d677d8c59840e94e3778f
RienNeVaPlus <?@rienneva.plus>
2021-11-13 06:43:03 +0100

    Improve: wordlist optimized (lower maxLength and higher poolSize)

--- index.ts
+++ index.ts
@@ -1,57 +1,61 @@
-export const adjectives: string[] = ["afraid", "all", "angry", "beige", "big", "bitter", "blue", "brave", "breezy", "bright", "brown", "bumpy", "busy", "calm", "chatty", "chilled", "chilly", "chubby", "clean", "clever", "cold", "cool", "crazy", "cruel", "cuddly", "curly", "curvy", "cute", "cyan", "deep", "dirty", "dry", "dull", "eager", "early", "easy", "eight", "eighty", "eleven", "empty", "every", "evil", "fair", "famous", "fast", "few", "fifty", "five", "flat", "fluffy", "forty", "four", "fresh", "friendly", "fruity", "funny", "fuzzy", "gentle", "giant", "gold", "good", "gorgeous", "great", "green", "grumpy", "happy", "healthy", "heavy", "hip", "honest", "hot", "hungry", "icy", "itchy", "khaki", "kind", "large", "late", "lazy", "lemon", "light", "little", "long", "loose", "loud", "lovely", "lucky", "many", "mean", "metal", "mighty", "modern", "moody", "nasty", "neat", "nervous", "new", "nice", "nine", "ninety", "odd", "old", "olive", "orange", "perfect", "pink", "plenty", "polite", "poor", "popular", "pretty", "proud", "puny", "purple", "quick", "quiet", "rare", "real", "red", "rich", "rotten", "rude", "sad", "salty", "selfish", "serious", "seven", "shaggy", "shaky", "sharp", "shiny", "short", "shy", "silent", "silly", "silver", "six", "sixty", "slick", "slimy", "slow", "small", "smart", "smooth", "soft", "solid", "some", "sour", "spicy", "spotty", "stale", "strange", "strong", "stupid", "sweet", "swift", "tall", "tame", "tangy", "tasty", "ten", "tender", "thick", "thin", "thirty", "three", "tidy", "tiny", "tired", "tough", "tricky", "twelve", "twenty", "two", "unlucky", "upset", "vast", "violet", "warm", "weak", "wet", "wicked", "wide", "wild", "wise", "witty", "yellow", "young", "yummy"];
+export const adjectives: string[] = ["afraid", "all", "angry", "beige", "big", "better", "bitter", "blue", "brave", "breezy", "bright", "brown", "bumpy", "busy", "calm", "chatty", "chilly", "chubby", "clean", "clear", "clever", "cold", "crazy", "cruel", "cuddly", "curly", "curvy", "cute", "common", "cold", "cool", "cyan", "dark", "deep", "dirty", "dry", "dull", "eager", "early", "easy", "eight", "eighty", "eleven", "empty", "every", "evil", "fair", "famous", "fast", "fancy", "few", "fine", "fifty", "five", "flat", "fluffy", "floppy", "forty", "four", "free", "fresh", "fruity", "full", "funny", "fuzzy", "gentle", "giant", "gold", "good", "great", "green", "grumpy", "happy", "heavy", "hip", "honest", "hot", "huge", "hungry", "icy", "itchy", "khaki", "kind", "large", "late", "lazy", "lemon", "legal", "light", "little", "long", "loose", "loud", "lovely", "lucky", "major", "many", "mean", "metal", "mighty", "modern", "moody", "nasty", "neat", "new", "nice", "nine", "ninety", "odd", "old", "olive", "open", "orange", "pink", "plain", "plenty", "polite", "poor", "pretty", "proud", "public", "puny", "petite", "purple", "quick", "quiet", "rare", "real", "ready", "red", "rich", "ripe", "rotten", "rude", "sad", "salty", "seven", "shaggy", "shaky", "sharp", "shiny", "short", "shy", "silent", "silly", "silver", "six", "sixty", "slick", "slimy", "slow", "small", "smart", "smooth", "social", "soft", "solid", "some", "sour", "spicy", "spotty", "stale", "strong", "stupid", "sweet", "swift", "tall", "tame", "tangy", "tasty", "ten", "tender", "thick", "thin", "thirty", "three", "tidy", "tiny", "tired", "tough", "tricky", "true", "twelve", "twenty", "two", "upset", "vast", "violet", "warm", "weak", "wet", "whole", "wicked", "wide", "wild", "wise", "witty", "yellow", "young", "yummy"]
 
-export const nouns: string[] = ["apes", "animals", "bars", "baths", "breads", "bushes", "cloths", "clubs", "hoops", "loops", "memes", "papers", "parks", "paths", "showers", "signs", "sites", "streets", "teeth", "tires", "webs", "actors", "ads", "adults", "aliens", "ants", "apples", "apricots", "avocados", "baboons", "badgers", "bags", "balloons", "bananas", "bats", "beans", "bears", "beds", "beers", "bees", "berries", "bikes", "birds", "boats", "bobcats", "books", "bottles", "boxes", "brooms", "buckets", "bugs", "bulldogs", "buses", "buttons", "camels", "cameras", "candies", "candles", "carpets", "carrots", "carrots", "cars", "cats", "chairs", "cheetahs", "chefs", "cherries", "chicken", "clocks", "clouds", "coats", "cobras", "coins", "colts", "comics", "cooks", "cougars", "regions", "cows", "crabs", "crabs", "crews", "cups", "cycles", "dancers", "days", "deers", "dingos", "dodos", "dogs", "dolls", "dolphins", "donkeys", "donuts", "doodles", "doors", "dots", "dragons", "drinks", "dryers", "ducks", "ducks", "eagles", "ears", "eels", "eggs", "mammals", "emus", "experts", "eyes", "falcons", "fans", "feet", "files", "fireants", "fishes", "flies", "flowers", "forks", "foxes", "friends", "frogs", "games", "garlics", "geckos", "geese", "ghosts", "ghosts", "gifts", "glasses", "goats", "gorillas", "grapes", "guests", "hairs", "hats", "hornets", "horses", "hotels", "hounds", "houses", "humans", "icons", "impalas", "insects", "islands", "items", "jars", "jeans", "jobs", "jokes", "angaroos", "keys", "kids", "kings", "kiwis", "knives", "ladybugs", "lamps", "laws", "lemons", "lies", "ligers", "lions", "lizards", "llamas", "lobsters", "mails", "mangos", "maps", "masks", "mayflies", "meals", "melons", "mice", "mirrors", "moles", "monkeys", "months", "moons", "moose", "mugs", "nails", "needles", "news", "numbers", "olives", "onions", "oranges", "otters", "owls", "pandas", "pans", "panthers", "pants", "papayas", "parents", "parrots", "paws", "peaches", "pears", "peas", "penguins", "pens", "pets", "phones", "pianos", "pigs", "pillows", "planes", "planets", "plants", "plums", "poems", "poets", "points", "pots", "pugs", "pumas", "pumpkins", "queens", "rabbits", "radios", "rats", "ravens", "readers", "rice", "rings", "rivers", "rockets", "rocks", "roses", "rules", "schools", "scissors", "bats", "seals", "seas", "sheep", "shirts", "shoes", "shrimps", "singers", "sloths", "snails", "snakes", "socks", "spiders", "spies", "spoons", "squids", "stars", "steaks", "wings", "students", "suits", "suns", "swans", "symbols", "tables", "taxis", "teachers", "terms", "ties", "tigers", "timers", "tips", "toes", "tomatoes", "tools", "toys", "trainers", "trains", "trams", "trees", "turkeys", "turtles", "vans", "walls", "walls", "wasps", "waves", "ways", "weeks", "windows", "wolves", "wombats", "worms", "yaks", "years", "zebras", "zoos"];
+export const nouns: string[] = ["apes", "animals", "areas", "bars", "banks", "baths", "breads", "bushes", "cloths", "clowns", "clubs", "hoops", "loops", "memes", "papers", "parks", "paths", "showers", "sides", "signs", "sites", "streets", "teeth", "tires", "webs", "actors", "ads", "adults", "aliens", "ants", "apples", "baboons", "badgers", "bags", "bananas", "bats", "beans", "bears", "beds", "beers", "bees", "berries", "bikes", "birds", "boats", "bobcats", "books", "bottles", "boxes", "brooms", "buckets", "bugs", "buses", "buttons", "camels", "cases", "cameras", "candies", "candles", "carpets", "carrots", "carrots", "cars", "cats", "chairs", "chefs", "chicken", "clocks", "clouds", "coats", "cobras", "coins", "corners", "colts", "comics", "cooks", "cougars", "regions", "results", "cows", "crabs", "crabs", "crews", "cups", "cities", "cycles", "dancers", "days", "deer", "dingos", "dodos", "dogs", "dolls", "donkeys", "donuts", "doodles", "doors", "dots", "dragons", "drinks", "dryers", "ducks", "ducks", "eagles", "ears", "eels", "eggs", "ends", "mammals", "emus", "experts", "eyes", "facts", "falcons", "fans", "feet", "files", "flies", "flowers", "forks", "foxes", "friends", "frogs", "games", "garlics", "geckos", "geese", "ghosts", "ghosts", "gifts", "glasses", "goats", "grapes", "groups", "guests", "hairs", "hands", "hats", "heads", "hornets", "horses", "hotels", "hounds", "houses", "humans", "icons", "ideas", "impalas", "insects", "islands", "items", "jars", "jeans", "jobs", "jokes", "keys", "kids", "kings", "kiwis", "knives", "lamps", "lands", "laws", "lemons", "lies", "lights", "lines", "lions", "lizards", "llamas", "mails", "mangos", "maps", "masks", "meals", "melons", "mice", "mirrors", "moments", "moles", "monkeys", "months", "moons", "moose", "mugs", "nails", "needles", "news", "nights", "numbers", "olives", "onions", "oranges", "otters", "owls", "pandas", "pans", "pants", "papayas", "parents", "parts", "parrots", "paws", "peaches", "pears", "peas", "pens", "pets", "phones", "pianos", "pigs", "pillows", "places", "planes", "planets", "plants", "plums", "poems", "poets", "points", "pots", "pugs", "pumas", "queens", "rabbits", "radios", "rats", "ravens", "readers", "rice", "rings", "rivers", "rockets", "rocks", "rooms", "roses", "rules", "schools", "bats", "seals", "seas", "sheep", "shirts", "shoes", "shrimps", "singers", "sloths", "snails", "snakes", "socks", "spiders", "spies", "spoons", "squids", "stars", "states", "steaks", "wings", "suits", "suns", "swans", "symbols", "tables", "taxes", "taxis", "teams", "terms", "things", "ties", "tigers", "times", "tips", "toes", "towns", "tools", "toys", "trains", "trams", "trees", "turkeys", "turtles", "vans", "views", "walls", "walls", "wasps", "waves", "ways", "weeks", "windows", "wolves", "women", "wombats", "words", "worlds", "worms", "yaks", "years", "zebras", "zoos"]
 
-export const verbs: string[] = ["accept", "act", "add", "admire", "agree", "allow", "appear", "applaud", "approve", "argue", "arrive", "attack", "attend", "bake", "bathe", "battle", "beam", "beg", "begin", "behave", "boil", "bow", "brake", "breathe", "brush", "build", "burn", "buy", "call", "camp", "care", "carry", "change", "cheat", "check", "cheer", "chew", "clap", "clean", "collect", "compare", "compete", "complain", "confess", "cough", "count", "cover", "crash", "cross", "cry", "dance", "decide", "deliver", "deny", "design", "destroy", "develop", "divide", "do", "double", "doubt", "draw", "dream", "dress", "drive", "drop", "drum", "eat", "end", "enjoy", "exercise", "exist", "explain", "explode", "fail", "fetch", "film", "fix", "flash", "float", "flow", "fly", "fold", "fry", "give", "glow", "grab", "greet", "grin", "grow", "guess", "hammer", "hang", "happen", "heal", "hear", "help", "hide", "hope", "hug", "hunt", "impress", "invent", "invite", "itch", "jam", "jog", "join", "joke", "judge", "juggle", "jump", "kick", "kiss", "kneel", "knock", "know", "laugh", "lay", "learn", "leave", "lick", "lie", "listen", "live", "look", "love", "march", "marry", "mate", "matter", "melt", "mix", "move", "nail", "notice", "obey", "occur", "own", "pay", "peel", "perform", "play", "poke", "press", "pretend", "promise", "protect", "prove", "provide", "pull", "pump", "punch", "push", "raise", "reflect", "refuse", "relate", "relax", "remain", "remember", "repair", "repeat", "reply", "report", "rescue", "rest", "retire", "return", "rhyme", "ring", "roll", "rule", "run", "rush", "scream", "search", "sell", "serve", "shake", "share", "shave", "shop", "shout", "sin", "sing", "sip", "sit", "sleep", "smash", "smell", "smile", "smoke", "sneeze", "sniff", "sort", "sparkle", "speak", "stare", "study", "suffer", "swim", "switch", "talk", "tan", "tap", "taste", "teach", "tease", "tell", "thank", "think", "tickle", "tie", "trade", "train", "travel", "try", "turn", "type", "unite", "vanish", "visit", "wait", "walk", "warn", "wash", "watch", "wave", "whisper", "wink", "wonder", "work", "worry", "yawn", "yell"];
+export const verbs: string[] = ["accept", "act", "add", "admire", "agree", "allow", "appear", "argue", "arrive", "ask", "attack", "attend", "bake", "bathe", "battle", "beam", "beg", "begin", "behave", "bet", "boil", "bow", "brake", "brush", "build", "burn", "buy", "call", "camp", "care", "carry", "change", "cheat", "check", "cheer", "chew", "clap", "clean", "cough", "count", "cover", "crash", "create", "cross", "cry", "cut", "dance", "decide", "deny", "design", "dig", "divide", "do", "double", "doubt", "draw", "dream", "dress", "drive", "drop", "drum", "eat", "end", "enter", "enjoy", "exist", "fail", "fall", "feel", "fetch", "film", "find", "fix", "flash", "float", "flow", "fly", "fold", "follow", "fry", "give", "glow", "go", "grab", "greet", "grin", "grow", "guess", "hammer", "hang", "happen", "heal", "hear", "help", "hide", "hope", "hug", "hunt", "invent", "invite", "itch", "jam", "jog", "join", "joke", "judge", "juggle", "jump", "kick", "kiss", "kneel", "knock", "know", "laugh", "lay", "lead", "learn", "leave", "lick", "like", "lie", "listen", "live", "look", "lose", "love", "make", "march", "marry", "mate", "matter", "melt", "mix", "move", "nail", "notice", "obey", "occur", "open", "own", "pay", "peel", "play", "poke", "post", "press", "prove", "pull", "pump", "pick", "punch", "push", "raise", "read", "refuse", "relate", "relax", "remain", "repair", "repeat", "reply", "report", "rescue", "rest", "retire", "return", "rhyme", "ring", "roll", "rule", "run", "rush", "say", "scream", "see", "search", "sell", "send", "serve", "shake", "share", "shave", "shine", "show", "shop", "shout", "sin", "sink", "sing", "sip", "sit", "sleep", "slide", "smash", "smell", "smile", "smoke", "sneeze", "sniff", "sort", "speak", "spend", "stand", "start", "stay", "stick", "stop", "stare", "study", "suffer", "swim", "switch", "take", "talk", "tan", "tap", "taste", "teach", "tease", "tell", "thank", "think", "throw", "tickle", "tie", "trade", "train", "travel", "try", "turn", "type", "unite", "vanish", "visit", "wait", "walk", "warn", "wash", "watch", "wave", "wear", "win", "wink", "wish", "wonder", "work", "worry", "write", "yawn", "yell"]
 
-function random(arr: string[]): string { return arr[Math.floor(Math.random()*arr.length)]; }
-// function longest(arr: string[]){ return arr.reduce((a, b) => a.length > b.length ? a : b); }
-// function shortest(arr: string[]){ return arr.reduce((a, b) => a.length < b.length ? a : b); }
+function random(arr: string[]): string { return arr[Math.floor(Math.random()*arr.length)] }
+// function longest(arr: string[]){ return arr.reduce((a, b) => a.length > b.length ? a : b) }
+// function shortest(arr: string[]){ return arr.reduce((a, b) => a.length < b.length ? a : b) }
 
-// Save some time by hard coding the minLength
-export const minLength: number = 8;
+// save some time by hard coding the minLength
+export const minLength: number = 8
 // [
 // 	shortest(adjectives).length,
 // 	shortest(nouns).length,
 // 	shortest(verbs).length
 // ].reduce((a,b) => a + b);
 
-// Save some time by hard coding the minLength
-export const maxLength: number = 24;
+// save some time by hard coding the maxLength
+export const maxLength: number = 19
 // [
 // 	longest(adjectives).length,
 // 	longest(nouns).length,
 // 	longest(verbs).length
-// ].reduce((a,b) => a + b);
+// ].reduce((a,b) => a + b)
 
-export const poolSize: number = adjectives.length * nouns.length * verbs.length;
+export const poolSize: number = adjectives.length * nouns.length * verbs.length
 
 interface Options {
 	separator?: string
 	capitalize?: boolean
+	multiplyPool?: number
 }
 
 export function humanId(options: Options | string = {}): string {
-	if(typeof options === 'string') options = { separator: options };
-	const { separator = '', capitalize = true } = options;
+	if(typeof options === 'string') options = { separator: options }
+	const { separator = '', capitalize = true } = options
 
 	let res = [
 		random(adjectives),
 		random(nouns),
 		random(verbs)
-	];
+	]
 
 	if(capitalize)
-		res = res.map(r => r.charAt(0).toUpperCase() + r.substr(1));
+		res = res.map(r => r.charAt(0).toUpperCase() + r.substr(1))
 
-	return res.join(separator);
+	return res.join(separator)
 }
 
-export default humanId;
+export default humanId
 
-// console.log('adjectives', adjectives.length);
-// console.log('nouns', nouns.length);
-// console.log('verbs', verbs.length);
-// console.log('poolSize', poolSize);
-// console.log('minLength', minLength);
-// console.log('maxLength', maxLength);
+// console.log('longest adjective', longest(adjectives), longest(adjectives).length)
+// console.log('longest noun', longest(nouns), longest(nouns).length)
+// console.log('longest verb', longest(verbs), longest(verbs).length)
+// console.log('adjectives', adjectives.length)
+// console.log('nouns', nouns.length)
+// console.log('verbs', verbs.length)
+// console.log('poolSize', poolSize)
+// console.log('minLength', minLength)
+// console.log('maxLength', maxLength)
